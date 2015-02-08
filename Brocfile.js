78ad95e7f5c8fdaa8004b828e3616d9aaaa273df
Marcel Erz <marcel.erz@gmail.com>
2015-02-07 20:17:25 -0800

    Ported TODOMVC from Ember.js to Ember-CLI

--- Brocfile.js
+++ Brocfile.js
@@ -16,5 +16,10 @@ var app = new EmberApp();
 // modules that you would like to import into your application
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
+//app.import('bower_components/ember-localstorage-adapter/localstorage_adapter.js');
+
+app.import('bower_components/todomvc-common/base.css');
+app.import('bower_components/todomvc-common/base.js');
+app.import('bower_components/todomvc-common/bg.png', { destDir: 'assets' });
 
 module.exports = app.toTree();
